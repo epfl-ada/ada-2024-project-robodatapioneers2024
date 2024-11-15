e3a45347120bb4f6b8386e9f271e8845fe4e275e
andres <andresnowak52@gmail.com>
2024-11-15 23:28:23 +0100

    modified keyword searcher

--- helpers.py
+++ helpers.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import re
 
-def keyword_searcher(df, keywords):
+def keyword_searcher(df, keywords, columns):
     pattern = '|'.join([r'\b' + re.escape(keyword) + r'\b' for keyword in keywords])
 
     return df[
-        df.apply(lambda row: bool(re.search(pattern, row['tags'], re.IGNORECASE)) or bool(re.search(pattern, row['title'], re.IGNORECASE)), axis=1)
+        df.apply(lambda row: any(bool(re.search(pattern, row[col], re.IGNORECASE)) for col in columns), axis=1)
     ].copy()
