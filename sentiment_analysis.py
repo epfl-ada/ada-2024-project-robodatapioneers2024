6c41833c9e05a618b6ca0a0ca788c572760e86bb
andres <andresnowak52@gmail.com>
2024-12-13 20:40:48 +0100

    updated sentiment_analysis

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -23,15 +23,30 @@ def analyze_sentiment_in_batches(sentiment_task, df, column_names, batch_size=BA
             batch_text, verbose=True, batch_size=512, truncation=True, max_length=MAX_LENGTH
         )
 
+        results_title = sentiment_task(
+            batch[column_names[0]], verbose=True, batch_size=512, truncation=True, max_length=MAX_LENGTH
+        )
+        results_description = sentiment_task(
+            batch[column_names[1]], verbose=True, batch_size=512, truncation=True, max_length=MAX_LENGTH
+        )
+
         batch['sentiment'] = [item['label'] for item in results]
         batch['sentiment_score'] = [item['score'] for item in results]
+        batch['sentiment_title'] = [item['label'] for item in results_title]
+        batch['sentiment_score_title'] = [item['score'] for item in results_title]
+        batch['sentiment_description'] = [item['label'] for item in results_description]
+        batch['sentiment_score_description'] = [item['score'] for item in results_description]
 
         return batch
 
     results = dataset.map(process_batch, batched=True, batch_size=batch_size)
 
-    df['sentiment'] = np.concatenate([result['sentiment'] for result in results])
-    df['sentiment_score'] = np.concatenate([result['sentiment_score'] for result in results])
+    df['sentiment'] = [result['sentiment'] for result in results]
+    df['sentiment_score'] = [result['sentiment_score'] for result in results]
+    df['sentiment_title'] = [result['sentiment_title'] for result in results]
+    df['sentiment_score_title'] = [result['sentiment_score_title'] for result in results]
+    df['sentiment_description'] = [result['sentiment_description'] for result in results]
+    df['sentiment_score_description'] = [result['sentiment_score_description'] for result in results]
 
     return df
 
@@ -48,16 +63,15 @@ def main():
     print("loading data...")
     filtered_df_sport_category = pd.read_parquet(
         'filtered_sport_category_metadata.parquet')
+    
 
     print("Classifying...")
     filtered_df_sport_category = analyze_sentiment_in_batches(
-        sentiment_task, filtered_df_sport_category, ["title", 'description'], 4096)
+        sentiment_task, filtered_df_sport_category, ["title", 'description'], 8192)
 
     print("Save...")
-    filtered_df_sport_category = filtered_df_sport_category.drop(
-        columns=['description'])
     filtered_df_sport_category.to_parquet(
-        "filtered_sport_category_with_sentiment_and_without_description_column_metadata.parquet")
+        "filtered_sport_category_with_sentiment_column_metadata.parquet")
 
 
 if __name__ == "__main__":
