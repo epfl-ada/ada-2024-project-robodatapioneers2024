5592c17805739f2360e2abc9cc8500836afe5482
LeThaoHuyen <lethaohuyen3010@gmail.com>
2024-12-20 00:32:36 +0100

    Add function for gettins all tags as a list

---
+++ src/utils/tag_utils.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import re
+
+def get_processed_tags(df: pd.DataFrame) -> list[str]:
+    df['tags'] = df['tags'].str.lower()
+    df['tags'] = df['tags'].str.replace("world cup", "world_cup")
+    df['tags'] = df['tags'].str.replace("table tennis", "table_tennis")
+    df['tags'] = df['tags'].str.replace("ping pong", "table_tennis")
+    
+    all_tags = []
+    for tags in df['tags']:
+        tag_list = tags.split(",")
+        for sentence in tag_list:
+            sentence = re.sub(r'[^\w\s]', '', sentence)
+            all_tags.extend(word_tokenize(sentence))
+            
+    return all_tags
