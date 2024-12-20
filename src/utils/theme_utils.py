c8474e51aa1c46d84419b46662a66621bf159032
LeThaoHuyen <lethaohuyen3010@gmail.com>
2024-12-20 15:35:48 +0100

    Add more colors

--- src/utils/theme_utils.py
+++ src/utils/theme_utils.py
@@ -3,12 +3,18 @@ from matplotlib.colors import LinearSegmentedColormap
 
 # Color palette
 colors = [
-  "#e50000", 
-  "#165B33", 
-  "#187F42", 
-  "#f8b229", 
-  "#91060d"
+  "#e50000", # red
+  "#165B33", # green
+  "#187F42", # light green
+  "#f8b229", # yellow
+  "#91060d" # dark red
 ]
 
+colors_pastel = [
+    "#558a6a"
+    "#97bd88",
+    "#e8e791",
+    "#e0878a",
+]
 # Create a custom colormap
 CUSTOM_CMAP = LinearSegmentedColormap.from_list("custom_palette", colors)
